9dfc35b4d5b2bdb00c252b7df3ab34534e568cba
jiiimni <leejimin8951@naver.com>
2026-02-05 01:21:04 +0900

    init project

---
+++ src/main/java/kr/spartaclub/scheduleapp/entity/schedule.java
@@ -0,0 +1,4 @@
+package kr.spartaclub.scheduleapp.entity;
+
+public class schedule {
+}
---
+++ src/test/java/kr/spartaclub/scheduleapp/ScheduleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.spartaclub.scheduleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
